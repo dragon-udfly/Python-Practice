39064539f6a487f8303df8d7e7c213be47093384
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-03 13:32:52 +0530

    Initialized attributes

--- CreateClass.py
+++ CreateClass.py
@@ -1,9 +1,11 @@
 class Item: 
-    def __init__(self, name):
+    def __init__(self, name, price, quantity):
         self.name= name
+        self.price= price 
+        self.quantity= quantity
 
     def calculate_price(self):
         pass
 
-item1= Item("Dragon")
-print(f"Name: {item1.name}")
+item1= Item("Dragon", 1000, 3)
+print(f"Name: {item1.name} \nPrice: {item1.price} \nQuantity: {item1.quantity}")
