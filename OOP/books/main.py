73f6545dc73d6735bf9f46b73d7df4ef5c5221a0
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-17 00:18:56 +0530

    Add example usage of Book class in main module

---
+++ OOP/books/main.py
@@ -0,0 +1,6 @@
+# import Book
+from Book import Book
+
+# book = Book.Book("Any Title", "Any Author", "Any Category", False, 32.3)
+book = Book("Any Title", "Any Author", "Any Category", False, 32.3)
+book.print_book()
