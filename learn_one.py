b45857949cd5bf39b004a499dc1dbfd0a8e5aa1e
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 20:30:27 +0530

    Add basic program to display personal information

---
+++ learn_one.py
@@ -0,0 +1,18 @@
+print("This is a basic program.")
+
+person_name = "Silvia"
+person_age = 32
+person_height = 1.3
+person_weight = 59.3
+person_married = False
+person_grade = 'C'
+person_address = "New York, 32 Mile Road, 53 Block"
+
+
+print(f"Name: {person_name}")
+print(f"Age: {person_age}")
+print(f"Is Married: {person_married}")
+print(f"Grade: {person_grade}")
+print(f"Address: {person_address}")
+print(f"Height: {person_height}")
+print(f"Weight: {person_weight}")
