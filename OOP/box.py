a306464ad6c8d1a31893539d1e44cd129d68980d
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 23:48:29 +0530

    Refactor Box class to improve encapsulation with property getters and setters for height, width, and length

--- OOP/box.py
+++ OOP/box.py
@@ -6,6 +6,38 @@ class Box:
         self.__width = width 
         self.__length = length
 
+    # creating a getter
+    @property
+    def height(self):
+        """Returns the height."""
+        return self.__height
+    
+    # creating setter
+    @height.setter
+    def height(self, height):
+        """Change height of the box."""
+        self.__height = height
+
+    @property 
+    def width(self):
+        """Return the width."""
+        return self.__width
+    
+    @width.setter 
+    def width(self, width):
+        """Change width of the box."""
+        self.__width = width
+
+    @property 
+    def length(self):
+        """Returns the length."""
+        return self.__length
+    
+    @length.setter 
+    def length(self, length):
+        """Change the length."""
+        self.__length = length
+
     def box_area(self):
         """Calculate area of the box."""
         return (6 * self.__width * self.__height)
@@ -24,3 +56,6 @@ box_one_volume = box_one.box_volume()
 
 print(f"Box One Area: {box_one_area}")
 print(f"Box One Volume: {box_one_volume}")
+
+box_two.height = 32.3
+print(f"Height of Box Two: {box_two.height}")
