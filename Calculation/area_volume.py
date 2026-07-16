331aeb57b6ef27dc2e8be2ff91a08d16ef738fab
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 20:46:05 +0530

    Add calculations for area and volume of circle related objects

---
+++ Calculation/area_volume.py
@@ -0,0 +1,13 @@
+# calculating area and volume of circle related objects
+
+PI = 3.143
+
+
+# Circle 
+circle_diameter = 32.1
+cirlce_circumstance = PI * (circle_diameter / 2)
+circle_area = PI * ((circle_diameter * circle_diameter / 4) * (circle_diameter * circle_diameter / 4))
+
+print(f"Circle Diameter: {circle_diameter}m")
+print(f"Circle Circumstance: {cirlce_circumstance}m")
+print(f"Cricle Area: {circle_area}m^2")
