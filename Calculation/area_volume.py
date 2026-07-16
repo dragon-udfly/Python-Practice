fdcdf9563876fa60e7068a69517ff1c73ac8d6cb
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 20:52:37 +0530

    Fix typo in cylinder area calculation

--- Calculation/area_volume.py
+++ Calculation/area_volume.py
@@ -15,7 +15,7 @@ print(f"Cricle Area: {circle_area}m^2")
 # Cylinder 
 cylinder_diameter = 45.2
 cylinder_height = 78.3
-cylinder_area = (PI * (cylinder_diameter * cylinder_diameter / 4) + 2 * PI (cylinder_diameter / 2) * cylinder_height)
+cylinder_area = (PI * (cylinder_diameter * cylinder_diameter / 4) + 2 * PI * (cylinder_diameter / 2) * cylinder_height)
 cylinder_volume = PI * (cylinder_diameter * cylinder_diameter / 4) * cylinder_height
 print(f"Cylinder Diameter {cylinder_diameter}m")
 print(f"Cylinder Height: {cylinder_height}m")
