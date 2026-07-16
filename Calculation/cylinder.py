6c8e0dadc938b360e597fecf8826b73ed17d9fd0
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 23:05:37 +0530

    Add calculations for area, circumference, and volume of cylinder

---
+++ Calculation/cylinder.py
@@ -0,0 +1,27 @@
+# calculate Area, circumstance and volume of the cylinder
+
+PI = 3.143
+
+def cylinder_area(diameter, height):
+    return ((PI * (diameter * diameter / 4)) + (2 * PI * (diameter / 2) * height))
+
+def cylinder_volume(diameter, height):
+    return (PI * (diameter * diameter / 4) * height)
+
+def cylider_circumference(diameter):
+    return (PI * diameter)
+
+
+# Take user inputs 
+
+diameter = float(input("Enter Diameter: "))
+height = float(input("Enter Height: "))
+
+area = cylinder_area(diameter, height)
+print(f"Area of Cylinder: {area}")
+
+volume = cylinder_volume(diameter, height)
+print(f"Volume of Cylinder: {volume}")
+
+circumference = cylider_circumference(diameter)
+print(f"Circumference of Cylinder: {circumference}")
