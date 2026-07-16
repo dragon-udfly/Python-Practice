162e74a614343a9c508215ebf0e0defe899bf8c4
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-16 23:02:36 +0530

    Fix typo in circle circumference calculation

--- Calculation/area_volume.py
+++ Calculation/area_volume.py
@@ -5,11 +5,11 @@ PI = 3.143
 
 # Circle 
 circle_diameter = 32.1
-cirlce_circumstance = PI * (circle_diameter / 2)
+cirlce_circumference = PI * (circle_diameter / 2)
 circle_area = PI * (circle_diameter * circle_diameter / 4)
 
 print(f"Circle Diameter: {circle_diameter}m")
-print(f"Circle Circumstance: {cirlce_circumstance}m")
+print(f"Circle circumference: {cirlce_circumference}m")
 print(f"Cricle Area: {circle_area}m^2\n")
 
 # Cylinder 
