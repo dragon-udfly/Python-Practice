6fe1148b2b9b2f874dfd43a852343cf5fd5bcff7
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:11:37 +0530

    Find Last Occurance

--- string.py
+++ string.py
@@ -4,9 +4,18 @@ length = len(text)
 print(f"Length of text: {length}\n")
 
 # find first occurance of given sub text
+# -1 if not found
 occ_space = text.find("xz ")
 if occ_space > 0:
     print(f"First Occurance of Space: {occ_space}\n")
 else:
     print("Not Found\n")
+
+# find last occurance of given sub text 
+# -1 if not found
+last_occ = text.rfind(" ")
+if last_occ > 0:
+    print(f"Last Occurance: {last_occ}\n")
+else: 
+    print("Not Found.\n")
 
