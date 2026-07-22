77aab562878795199b8617c43251b4df83d520b9
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:02:50 +0530

    Conditional Statement

---
+++ prog.py
@@ -0,0 +1,14 @@
+person_A_age = 23
+person_B_age = 24
+
+elder_age = person_A_age if person_A_age > person_B_age else person_B_age
+
+print(f"Elder Age: {elder_age}")
+
+in_a = int(input("Enter A: "))
+in_b = int(input("Enter B: "))
+
+max = in_a if in_a > in_b else in_b 
+
+print(f"Max: {max}")
+
