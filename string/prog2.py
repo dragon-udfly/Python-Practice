4998cb86d4b2d4a1dcdda0ef1de9fe71a99633cf
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:27:44 +0530

    checking whether string is only alphabetical

--- string/prog2.py
+++ string/prog2.py
@@ -6,3 +6,8 @@ text2 = " "
 print(f"Sample: {text}\tisDigit: {text.isdigit()}")
 print(f"Sample: {text1}\tisDigit: {text1.isdigit()}")
 print(f"Sample: {text2}\tisDigit: {text2.isdigit()}")
+
+word = "typewriter"
+# string only have alphabetical characters.
+print(f"Sample: {word}\tisAlphabetical: {word.isalpha()}")
+print(f"Sample: {text}\tisAlphabetical: {text.isalpha()}")
