3be2d18e3a502cd45eeeac394bfed98268199852
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:24:56 +0530

    checking string is only digit

---
+++ string/prog2.py
@@ -0,0 +1,8 @@
+text = "2323233"
+text1 = "232 323f"
+text2 = " "
+
+# the string contains only numbers
+print(f"Sample: {text}\tisDigit: {text.isdigit()}")
+print(f"Sample: {text1}\tisDigit: {text1.isdigit()}")
+print(f"Sample: {text2}\tisDigit: {text2.isdigit()}")
