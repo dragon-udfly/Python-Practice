76c58f319af2bc98d7e6c56184b25deef42d8fcf
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 18:06:53 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,13 @@
+print("x: ", end= " ")
+
+# for loop
+for x in range(10): 
+    print(x, end= " ")
+
+print("\n")
+
+# reversed range loop
+for y in reversed(range(1, 11)):
+    print(y, end = " ")
+
+print("\n")
