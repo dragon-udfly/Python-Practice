a4d697c9fe8849da8d8f5a846cfb149847bd8a9d
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:55:26 +0530

    thousand separator , and 2 decimal places

---
+++ format.py
@@ -0,0 +1,10 @@
+number = 32.300239023
+
+# print to 2 decimal places
+print(f"Number: {number:.2f}")
+
+# print to 3 decimal places 
+print(f"Number: {number:.3f}")
+
+balance = 3239920033.33
+print(f"Balance: {balance:,.2f}") # thousand separator , and 2 decimal places
