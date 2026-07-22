aee0f143535f073d5a0b1bfe1aa8e1bd6c5e2327
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:07:10 +0530

    First occurance of sub string

--- string.py
+++ string.py
@@ -2,3 +2,7 @@ text = "This is nothing abd3023 3002"
 
 length = len(text)
 print(f"Length of text: {length}\n")
+
+# find first occurance of given sub text
+occ_space = text.find(" ")
+print(f"First Occurance of Space: {occ_space}\n")
