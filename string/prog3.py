b4b7916c8e39f66eefaa470e3edb13d74a955710
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 17:42:17 +0530

    String slicing

---
+++ string/prog3.py
@@ -0,0 +1,9 @@
+# string slicing 
+text = "this is a text"
+substring_one = text[:5] # from begining to fifth
+substring_two = text[2:9] # from third character to nineth
+substring_three = text[5:] # from 6 th character to end
+
+print(f"Substring: {substring_one}")
+print(f"Substring: {substring_two}")
+print(f"Substring: {substring_three}")
