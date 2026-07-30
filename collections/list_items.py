c07e31af344ad321db9a1f17cff87aad73d4ccb2
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-30 12:02:33 +0530

    Itereate items in a list using for loop

--- collections/list_items.py
+++ collections/list_items.py
@@ -3,3 +3,6 @@ fruits = ["orange", "apple", "banana", "mango", "woodapple"]
 print(f"Fruits: \n{fruits}")
 print(f"Item: {fruits[2]}")
 print(f"Item: {fruits[3]}")
+
+for item in fruits:
+    print(item, end = " ")
