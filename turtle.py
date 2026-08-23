5acc4567194e9d286b533c6c3d4f145268a3c3dd
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-08-24 00:31:14 +0530

    Update numbers array in Turtle class and add sliceNumbers method for enhanced functionality

--- turtle.py
+++ turtle.py
@@ -11,9 +11,9 @@ class Turtle:
         self.word = ""
 
         self.numbers = np.array([
-            [[3, 3, 2], [2, 3, 0]], 
-            [[3, 6, 1], [1, 0, 2]], 
-            [[3, 5, 2], [4, 2, 5]]
+            [[3, 3, 2, 2, 3, 0], [4, 5, 9, 4, 3, 0]], 
+            [[3, 6, 1, 1, 0, 2], [6, 3, 2, 2, 3, 7]], 
+            [[3, 5, 2, 4, 2, 5], [8, 7, 1, 3, 3, 0]]
         ])
     
     def makeWord(self): 
@@ -26,10 +26,15 @@ class Turtle:
     def printShape(self): 
         print(f"Shape: {self.letters.shape}")
 
+    def sliceNumbers(self):
+        print(f"Result1: \n{self.numbers[1:]}")
+        print(f"Result2: \n{self.numbers[0:2:2]}") 
+        print(f"Result3: \n{self.numbers[0:2][0]}") 
+        # slicing & accessing element
+        print(f"Result4: \n{self.numbers[0:2][0][0][0]}")
+
 
 if __name__ == "__main__": 
     t1 = Turtle() 
-    t1.printDimension()
-    t1.printShape()
-
-    print(f"Word: {t1.makeWord()}")
+ 
+    t1.sliceNumbers()
