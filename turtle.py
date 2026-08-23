339d80525b9418f971363b1a05ab4465eb085fcb
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-08-24 00:08:13 +0530

    Add Turtle class with numpy array initialization and dimension/shape printing methods

---
+++ turtle.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+class Turtle: 
+    """Turtle blueprint"""
+
+    def __init__(self): 
+        self.letters = np.array([
+            [['A', 'C', 'R'], ['A', 'B', 'W']],
+            [['N', 'U', 'T'], ['G', 'Q', 'O']]
+        ])
+        self.word = ""
+    
+    def printDimension(self): 
+        print(f"Dimension: {self.letters.ndim}")
+
+    def printShape(self): 
+        print(f"Shape: {self.letters.shape}")
+
+
+t1 = Turtle() 
+t1.printDimension()
+t1.printShape()
